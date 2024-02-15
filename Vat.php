9a25b62dc51be1963a888f2b578dcb6c9647fc73
Shakira47971 <159004578+Shakira47971@users.noreply.github.com>
2024-02-15 16:47:19 +0600

    Add files via upload

---
+++ Vat.php
@@ -0,0 +1,9 @@
+<?php
+function Vat($x){
+         return($x*0.15);
+}
+$x=500;
+echo("Vat :");
+echo Vat($x);	 
+    
+	?>
---
+++ shape.php
@@ -0,0 +1,55 @@
+<html>
+    <head>
+        <title>shapes</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <td>
+<?php
+
+for($i=1;$i<=3;$i++){
+    
+	for($j=1;$j<=$i;$j++){
+		echo "*";
+	}
+    echo"br";
+	
+}
+?>
+</td>
+<td>
+<?php
+
+    
+for($i=3;$i>=1;$i--){
+    
+	for($j=1;$j<=$i;$j++){
+        echo"($j)." ";
+		
+	}
+    echo"br";
+	
+}
+?>
+</td>
+<td>
+<?php
+
+$count=65;
+for($i=1;$i<=3;$i++){
+    
+	for($j=1;$j<=$i;$j++){
+        echo"($count++)." ";
+		
+	}
+	echo"br";
+}
+?>
+</td>
+</tr>
+</table>
+	
+	?>
+    </body>
+    </html>
