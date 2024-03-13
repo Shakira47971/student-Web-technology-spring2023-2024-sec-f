f48032023cf896b563696fb999cb7f35d7338557
Shakira47971 <159004578+Shakira47971@users.noreply.github.com>
2024-03-13 23:44:39 +0600

    Add files via upload

---
+++ LAB-5.2(PHP Session  Tracking)/PublicHome.php
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your Title Here</title>
+</head>
+<body>    
+    <table height="400" width="800" border="1" style="border-collapse: collapse;">
+        <tr>
+            <td colspan="3" align="left">
+                <b><span style="Padding: 120px;font-size: 30px;">Company</span></b>
+                
+                
+                <a style="color:rgb(0, 102, 255);margin-top: 9px; padding:9px;" href="Home">Home</a>
+                <a style="color:rgb(0, 102, 255);margin-top: 9px; padding:9px;" href="Login">Login</a>
+                <a style="color:rgb(0, 102, 255);margin-top: 9px; padding:9px;" href="Registration">Registration</a>
+</td>
+            </td>
+        </tr>
+        <tr style="vertical-align: text-top;" height="300">
+        <td colspan="3" align="left">
+           <b> <span style="font-size: 20px;"> Welcome to xCompany</span></b>
+               
+            </td>
+            <td>
+                
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3" align="center">Copyright@2017</td>
+        </tr>
+    </table>
+</body>
+</html>
