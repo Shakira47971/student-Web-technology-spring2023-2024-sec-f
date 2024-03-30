2e37bad521d3421bbff1a46d4cf3f8f96fb302c5
Shakira47971 <159004578+Shakira47971@users.noreply.github.com>
2024-03-30 15:25:42 +0600

    Add files via upload

---
+++ MidLab/MidLabView/Topic3.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>Home</title>
+    </head>
+<body>
+<table border="1" cellspacing="0" width="550">
+
+        <tr  style="height:200;">
+            <td align="Left" style="width:200">
+       
+ 
+                    <span align="left">
+                        <ul>
+                            <li><a style="color:rgb(0, 102, 255);margin-top: 10px; padding:10px;" href="CV.php">Profile</a></li>
+                            <li><a style="color:rgb(0, 102, 255);margin-top: 10px; padding:10px;" href="Project.php">Project</a></li>
+                            <li><a style="color:rgb(0, 102, 255);margin-top: 10px; padding:10px;" href="Contact.php">Contact</a></li>
+                            <li> <a style="color:rgb(0, 102, 255);margin-top: 10px; padding:10px;" href="Photo.php">Album</a></li>
+                              </ul>
+</span>
+     
+                </td><td align=top><span align=top><b>Welcome to Shakira's Website</b></td>
+                </tr>
+                
+            </table>
+               
+</body>
+</html>
